c2e57683dfecd12d60e25e87979d3a113c25b249
JancarlosPol <jancarlospolanco167@gmail.com>
2025-04-10 17:27:02 -0400

    Create gradebook.js

---
+++ gradebook.js
@@ -0,0 +1,13 @@
+// Stub for fetching grade data (you'll implement this later)
+function fetchGradeData() {
+    console.log("fetchGradeData called");
+}
+
+// Stub for populating the gradebook table
+function populateGradebook() {
+    console.log("populateGradebook called");
+}
+
+// Call the stubs to verify they're connected to the HTML
+fetchGradeData();
+populateGradebook();
